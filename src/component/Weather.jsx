ee0690190dda7612dd93574cc19971a4064f602f
rooban295 <jrooban28@gmail.com>
2025-01-24 13:04:34 +0530

    changing the size

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -46,7 +46,7 @@ export const Weather = () => {
             setMsg(message);
             hide.current.style.top = '-50px';
             setTimeout(() => {
-            hide.current.style.top = '-150px';
+            hide.current.style.top = '-550px';
             }, 1000);
         };
 
@@ -94,13 +94,13 @@ export const Weather = () => {
 
         {
             weatherData?<>
-            <div ref={hide} className="absolute top-[-150px] left-[50px] border h-[40px] w-[70%] flex justify-between items-center bg-gradient-to-r from-[#6378a6]  to-[#103783] rounded-md px-5 duration-75">
+            <div ref={hide} className="absolute top-[-550px] left-[50px] border h-[40px] w-[70%] flex justify-between items-center bg-gradient-to-r from-[#6378a6]  to-[#103783] rounded-md px-5 duration-75">
             <p className='text-lg text-white'>{msg}</p>
             <AiOutlineCloseCircle className='text-red-600 h-7 w-7' onClick={()=>{hide.current.style.top='-150px'}}/>
             </div>
             </>:<></>
         }
-        <div className='place-self-center p-[20px] md:p-[40px] rounded-lg bg-gradient-to-r from-[#6378a6]  to-[#103783] flex flex-col items-center'>
+        <div className='place-self-center p-[10px] md:p-[40px] rounded-lg bg-gradient-to-r from-[#6378a6]  to-[#103783] flex flex-col items-center'>
 
             <div className="flex items-center gap-4 ">
                 <input ref={inputRef}  type="text" className="rounded-full p-1 border-none outline-none h-[50px] pl-[25px] text-[#626262] bg-[#ebfffc] text-xl" placeholder='Search' />
